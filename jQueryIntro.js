aa4d382168ebd4dc1adb789374cbf05c3009d864
BootCoder <bootcoder@gmail.com>
2015-05-23 23:38:22 -0700

    add boiler html - base jQuery notes

---
+++ jQueryIntro.js
@@ -0,0 +1,10 @@
+// jQuery === $
+// use standard CSS selectors .klass && #eyed && <h1>
+//
+console.log($('.klass'));
+console.log($('#eyed'));
+console.log($('h1'));
+//
+// can be set to a variable
+var domKlass = $('.klass');
+console.log(domKlass);

35d3831b0f0804374abfac0e8462c47fa69b0ab9
BootCoder <bootcoder@gmail.com>
2016-03-30 13:40:32 -0700

    add initial AJAX demo

--- js/0_main.js
+++ js/0_main.js
@@ -5,35 +5,47 @@
 
 
 $(document).ready(function () {
-
-  // js/1_getStarted.js
-
-  // basicSelection();
-  chainingCalls();
-  // manipulateDivs();
-  // iterationAndConditionals();
-
-
-
-
-  // // js/2_list.js
-
-  // delegatedListener();
-  // standardListener();
-  // addListItems();
-  // modListText();
-  // addNums();
-  // nthChild();
-  // listListener();
-
-
-
-
-  // // js/3_advanced.js
-
-  // picListener();
-  // picReturnListener();
-  // buttonListener();
-  // dynamicButtonListener();
-
+  // js1();
+  // js2();
+  // js3();
+  js4();
 });
+
+
+// js/1_getStarted.js
+var js1 = function(){
+  basicSelection();
+  chainingCalls();
+  manipulateDivs();
+  iterationAndConditionals();
+}
+
+
+
+
+// // js/2_list.js
+var js2 = function(){
+  delegatedListener();
+  standardListener();
+  addListItems();
+  modListText();
+  addNums();
+  nthChild();
+  listListener();
+}
+
+
+
+
+// js/3_advanced.js
+var js3 = function(){
+  picListener();
+  picReturnListener();
+  buttonListener();
+  dynamicButtonListener();
+}
+
+var js4 = function(){
+  appendGifSearch();
+  gifButtonListener();
+}
--- js/4_AJAX.js
+++ js/4_AJAX.js
@@ -1,1 +1,57 @@
 console.log("ready in AJAX land");
+
+// This function appends the form for searching Giphy.com to the DOM.
+var appendGifSearch = function(){
+  var formMarkup = `<form id="gif_search_form">
+                      <input type="text" name="search_term" placeholder="Enter search terms here."></input>
+                      <input type="submit" value="Find GIFs"></input>
+                    </form>`
+  // console.log(`${formMarkup}`);
+  $('#ajax_land').append(formMarkup);
+}
+
+// These are my helper methods, things that assist in getting the
+// job done while giving my code a cleaner more single responsibility feel.
+
+var buildImageElement = function(img_url){
+  return `<img class="col-md-3 ajax-gallery" src="${img_url}">`
+}
+
+var appendGifs = function(response_data) {
+  var img_container = '<div id="ajax_image_container"></div>'
+  $('#ajax_image_container').remove();
+  $('#ajax_land').append(img_container);
+
+  for (var i = 0; i < 6; i++) {
+    var image_element = buildImageElement(response_data.data[i].images.fixed_height_small.url);
+    $('#ajax_image_container').append(image_element);
+  }
+}
+
+// This is the core AJAX function which consumes the helper functions
+var gifButtonListener = function(){
+  $('#ajax_land').on('submit', '#gif_search_form', function(event){
+
+    event.preventDefault();
+
+    var baseUrl = "http://api.giphy.com/v1/gifs/"
+    var apiKey = "&api_key=dc6zaTOxFJmzC"
+    var formInput = $('#gif_search_form input').val()
+    var search = `search?q=${formInput}`
+
+    var giphyRequest = $.ajax({
+      url: baseUrl+search+apiKey
+    })
+
+    giphyRequest.done(function(response_data){
+      appendGifs(response_data);
+      console.log(response_data);
+    })
+
+    giphyRequest.fail(function(response_data){
+      console.log(response_data);
+      console.log('Ohh nooo');
+    })
+
+  })
+}
